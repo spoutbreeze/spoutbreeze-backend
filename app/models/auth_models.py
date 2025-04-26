0848411363fc0a1f262220663db59ed10066fc3e
firas-ben-said <firas.ben.said@horizon-tech.tn>
2025-04-26 14:17:28 +0100

    Added TokenRequest and TokenResponse models

--- app/models/auth_models.py
+++ app/models/auth_models.py
@@ -5,7 +5,7 @@ class TokenRequest(BaseModel):
     """
     Model for token exchange request
     """
-    token: str = Field(..., description="Authorization code from Keycloak")
+    code: str = Field(..., description="Authorization code from Keycloak")
     redirect_uri: str = Field(..., description="Redirect URI used in the authorization request")
 
 class TokenResponse(BaseModel):
@@ -31,3 +31,19 @@ class UserInfo(BaseModel):
     preferred_username: str
     email: Optional[str] = None
     full_name: Optional[str] = None
+
+class TokenRequests(BaseModel):
+    username: str
+    password: str
+
+
+class TokenResponses(BaseModel):
+    access_token: str
+    refresh_token: str
+    expires_in: int = None
+    refresh_expires_in: int = None
+    id_token: str
+    not_before_policy: int
+    session_state: str
+    scope: str
+    token_type: str = "Bearer"
