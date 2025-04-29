de38ebb44ee2a191ec34e13d17e1100f9dedc017
firas-ben-said <firas.ben.said@horizon-tech.tn>
2025-04-29 23:51:47 +0100

    Updated the CORS and deleted unnecessary code

--- app/main.py
+++ app/main.py
@@ -16,12 +16,13 @@ app = FastAPI(
 
 origins = [
     "http://localhost:3000",
-    "https://bbb3.riadvice.ovh"
+    "https://bbb3.riadvice.ovh",
+    "http://localhost:8080"
 ]
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, replace with specific origins
+    allow_origins=origins,  # In production, replace with specific origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -65,42 +66,4 @@ async def chat_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         chat_manager.disconnect(websocket)
         print("[Chat] Client disconnected")
-
-from fastapi import Depends, HTTPException, status, Request
-from keycloak.exceptions import KeycloakError
-from jose import JWTError
-from app.config.config import keycloak_openid
-from pydantic import BaseModel
-
-
-def get_current_user(request: Request):
-    auth_header = request.headers.get("Authorization")
-    if not auth_header or not auth_header.startswith("Bearer "):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
-
-    token = auth_header.split(" ")[1]
-
-    try:
-        # Verify and decode token
-        keycloak_openid.introspect(token)  # optional strict check
-        user_info = keycloak_openid.userinfo(token)
-        return user_info
-    except (KeycloakError, JWTError) as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token")
-
-
-class RefreshRequest(BaseModel):
-    refresh_token: str
-
-@app.post("/auth/refresh")
-def refresh_token(data: RefreshRequest):
-    try:
-        token = keycloak_openid.refresh_token(data.refresh_token)
-        return {
-            "access_token": token["access_token"],
-            "expires_in": token["expires_in"],
-            "refresh_token": token["refresh_token"],
-        }
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Refresh token invalid or expired")
 
