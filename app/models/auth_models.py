64e3ce806e47b50302ab4ced851b657951ce7357
firas-ben-said <firas.ben.said@horizon-tech.tn>
2025-05-05 11:48:42 +0100

    Updated the tokenResponse class in the models

--- app/models/auth_models.py
+++ app/models/auth_models.py
@@ -13,9 +13,10 @@ class TokenResponse(BaseModel):
     Model for token exchange response
     """
     access_token: str
-    refresh_token: str
-    token_type: str = "Bearer"
     expires_in: int
+    refresh_token: str
+    refresh_expires_in: Optional[int] = None
+    token_type: str = "Bearer"
     user_info: dict
 
 class User(BaseModel):
@@ -33,21 +34,6 @@ class UserInfo(BaseModel):
     email: Optional[str] = None
     full_name: Optional[str] = None
 
-# class TokenRequest(BaseModel):
-#     username: str
-#     password: str
-
-
-# class TokenResponse(BaseModel):
-#     access_token: str
-#     refresh_token: str
-#     expires_in: int = None
-#     refresh_expires_in: int = None
-#     id_token: str
-#     not_before_policy: int
-#     session_state: str
-#     scope: str
-#     token_type: str = "Bearer"
 
 class RefreshTokenRequest(BaseModel):
     refresh_token: str = Field(..., description="Refresh token to obtain new access token")
