e5d2c9cd7909d031d13b2e0e5cdc59bc36bb919e
firas-ben-said <firas.ben.said@horizon-tech.tn>
2025-05-05 11:45:06 +0100

    Implemented the crud operations for the stream enpoints

---
+++ app/controllers/stream_controller.py
@@ -0,0 +1,151 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List
+from uuid import UUID
+
+from app.config.database.session import get_db
+from app.controllers.user_controller import get_current_user
+from app.models.user_models import User
+from app.models.stream_schemas import (
+    StreamSettingsResponse,
+    StreamSettingsUpdate,
+    CreateStreamSettingsCreate,
+)
+from app.services.stream_service import StreamService
+
+router = APIRouter(prefix="/api/stream-endpoint", tags=["Stream Endpoints"])
+stream_service = StreamService()
+
+@router.post("/create", response_model=StreamSettingsResponse)
+async def create_stream_settings(
+    stream_settings: CreateStreamSettingsCreate,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+) -> StreamSettingsResponse:
+    """
+    Create a new stream settings for the current user.
+
+    Args:
+        stream_settings: The stream settings to create.
+        db: The database session.
+        current_user: The current user.
+
+    Returns:
+        The created stream settings.
+    """
+    try:
+        new_stream_settings = await stream_service.create_stream_settings(
+            stream_settings=stream_settings,
+            user_id=current_user.id,
+            db=db,
+        )
+        return new_stream_settings
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/", response_model=List[StreamSettingsResponse])
+async def get_stream_settings(
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+) -> List[StreamSettingsResponse]:
+    """
+    Get all stream settings for the current user.
+
+    Args:
+        db: The database session.
+        current_user: The current user.
+
+    Returns:
+        A list of stream settings.
+    """
+    try:
+        stream_settings = await stream_service.get_stream_settings_by_user_id(
+            user_id=current_user.id,
+            db=db,
+        )
+        return stream_settings
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/{stream_settings_id}", response_model=StreamSettingsResponse)
+async def get_stream_settings_by_id(
+    stream_settings_id: UUID,
+    db: AsyncSession = Depends(get_db),
+) -> StreamSettingsResponse:
+    """
+    Get stream settings by ID.
+
+    Args:
+        stream_settings_id: The ID of the stream settings.
+        db: The database session.
+
+    Returns:
+        The stream settings.
+    """
+    try:
+        stream_settings = await stream_service.get_stream_settings_by_id(
+            stream_settings_id=stream_settings_id,
+            db=db,
+        )
+        if not stream_settings:
+            raise HTTPException(status_code=404, detail="Stream settings not found")
+        return stream_settings
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.put("/{stream_settings_id}", response_model=StreamSettingsResponse)
+async def update_stream_settings(
+    stream_settings_id: UUID,
+    stream_settings_update: StreamSettingsUpdate,
+    db: AsyncSession = Depends(get_db),
+) -> StreamSettingsResponse:
+    """
+    Update stream settings by ID.
+
+    Args:
+        stream_settings_id: The ID of the stream settings.
+        stream_settings_update: The updated stream settings.
+        db: The database session.
+
+    Returns:
+        The updated stream settings.
+    """
+    try:
+        updated_stream_settings = await stream_service.update_stream_settings(
+            stream_settings_id=stream_settings_id,
+            stream_settings_update=stream_settings_update,
+            db=db,
+        )
+        if not updated_stream_settings:
+            raise HTTPException(status_code=404, detail="Stream settings not found")
+        return updated_stream_settings
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.delete("/{stream_settings_id}", response_model=StreamSettingsResponse)
+async def delete_stream_settings(
+    stream_settings_id: UUID,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+) -> StreamSettingsResponse:
+    """
+    Delete stream settings by ID.
+    Args:
+        stream_settings_id: The ID of the stream settings.
+        db: The database session.
+        current_user: The current user.
+    Returns:
+        The deleted stream settings.
+    """
+    try:
+        deleted_stream_settings = await stream_service.delete_stream_settings(
+            stream_settings_id=stream_settings_id,
+            user_id=current_user.id,
+            db=db,
+        )
+        if not deleted_stream_settings:
+            raise HTTPException(status_code=404, detail="Stream settings not found")
+        return deleted_stream_settings
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
---
+++ app/services/stream_service.py
@@ -0,0 +1,136 @@
+from app.models.stream_models import StreamSettings
+from app.models.stream_schemas import (
+    # StreamSettingsResponse,
+    StreamSettingsUpdate,
+    CreateStreamSettingsCreate,
+)
+from uuid import UUID
+from typing import List, Optional
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, update, delete
+from app.config.logger_config import logger
+
+
+class StreamService:
+    """
+    Service for creating the stream settings
+    """
+
+    async def create_stream_settings(
+        self,
+        stream_settings: CreateStreamSettingsCreate,
+        user_id: UUID,
+        db: AsyncSession,
+    ) -> StreamSettings:
+        """
+        Create a stream settings for a user
+        """
+        new_stream_settings = StreamSettings(
+            title=stream_settings.title,
+            stream_key=stream_settings.stream_key,
+            rtmp_url=stream_settings.rtmp_url,
+            user_id=user_id,
+        )
+        db.add(new_stream_settings)
+        await db.commit()
+        await db.refresh(new_stream_settings)
+
+        logger.info(
+            f"Stream settings with the name {new_stream_settings.title} created for user {user_id}"
+        )
+        return new_stream_settings
+
+    async def get_stream_settings_by_user_id(
+        self,
+        user_id: UUID,
+        db: AsyncSession,
+    ) -> List[StreamSettings]:
+        """
+        Get all stream settings for a user
+        """
+        stmt = select(StreamSettings).where(StreamSettings.user_id == user_id)
+        result = await db.execute(stmt)
+        stream_settings = result.scalars().all()
+
+        logger.info(f"Stream settings retrieved for user {user_id}")
+        return stream_settings
+
+    async def get_stream_settings_by_id(
+        self,
+        stream_settings_id: UUID,
+        db: AsyncSession,
+    ) -> Optional[StreamSettings]:
+        """
+        Get stream settings by ID
+        """
+        stmt = select(StreamSettings).where(StreamSettings.id == stream_settings_id)
+        result = await db.execute(stmt)
+        stream_settings = result.scalars().first()
+
+        logger.info(f"Stream settings retrieved with ID {stream_settings_id}")
+        return stream_settings
+
+    async def update_stream_settings(
+        self,
+        stream_settings_id: UUID,
+        stream_settings_update: StreamSettingsUpdate,
+        db: AsyncSession,
+    ) -> Optional[StreamSettings]:
+        """
+        Update stream settings by ID
+        """
+        # Check if the stream settings exist
+        stmt = select(StreamSettings).where(StreamSettings.id == stream_settings_id)
+        result = await db.execute(stmt)
+        stream_settings = result.scalars().first()
+        if not stream_settings:
+            logger.warning(f"Stream settings with ID {stream_settings_id} not found")
+            return None
+        
+        # Update the stream settings
+        stmt = (
+            update(StreamSettings)
+            .where(StreamSettings.id == stream_settings_id)
+            .values(
+                title=stream_settings_update.title,
+                rtmp_url=stream_settings_update.rtmp_url,
+                stream_key=stream_settings_update.stream_key,
+            )
+        )
+        await db.execute(stmt)
+        await db.commit()
+        await db.refresh(stream_settings)
+        logger.info(
+            f"Stream settings with ID {stream_settings_id} updated for user {stream_settings.user_id}"
+        )
+        return stream_settings
+    
+    async def delete_stream_settings(
+        self,
+        stream_settings_id: UUID,
+        user_id: UUID,
+        db: AsyncSession,
+    ) -> Optional[StreamSettings]:
+        """
+        Delete stream settings by ID
+        """
+        # Check if the stream settings exist
+        stmt = select(StreamSettings).where(
+            StreamSettings.id == stream_settings_id,
+            StreamSettings.user_id == user_id,
+        )
+        result = await db.execute(stmt)
+        stream_settings = result.scalars().first()
+        if not stream_settings:
+            logger.warning(f"Stream settings with ID {stream_settings_id} not found")
+            return None
+
+        # Delete the stream settings
+        stmt = delete(StreamSettings).where(StreamSettings.id == stream_settings_id)
+        await db.execute(stmt)
+        await db.commit()
+        logger.info(
+            f"Stream settings with ID {stream_settings_id} deleted for user {user_id}"
+        )
+        return stream_settings
