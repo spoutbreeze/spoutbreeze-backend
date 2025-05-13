1178fa6cfd0f9c7eeb3021b8a0f94d5f60f1ebe9
firas-ben-said <firas.ben.said@horizon-tech.tn>
2025-05-13 03:00:07 +0100

    Updated channel namein the modal to make it unique

--- app/models/channel/channels_model.py
+++ app/models/channel/channels_model.py
@@ -20,7 +20,7 @@ class Channel(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
     )
-    name: Mapped[str] = mapped_column(String, nullable=False)
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
     updated_at: Mapped[datetime] = mapped_column(
         DateTime, default=datetime.now(), onupdate=datetime.now()
